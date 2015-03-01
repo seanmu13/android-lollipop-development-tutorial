f46a53f7d466113c241bc446af87e6df2e10605e
Sean Murray <seanmu13@gmail.com>
2015-03-01 14:12:56 -0500

    Lecture 127

---
+++ app/src/main/java/org/seanmu13/flickrbrowser/BaseActivity.java
@@ -0,0 +1,31 @@
+package org.seanmu13.flickrbrowser;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.Toolbar;
+
+/**
+ * Created by seanmu13 on 2/26/2015.
+ */
+public class BaseActivity extends ActionBarActivity {
+
+    private Toolbar mToolbar;
+    public static final String FLICKR_QUERY = "FLICKR QUERY";
+
+    protected Toolbar activateToolbar() {
+        if(mToolbar == null) {
+            mToolbar = (Toolbar) findViewById(R.id.app_bar);
+            if(mToolbar != null){
+                setSupportActionBar(mToolbar);
+            }
+        }
+        return mToolbar;
+    }
+
+    protected Toolbar activateToolbarWithHomeEnabled(){
+        activateToolbar();
+        if(mToolbar != null){
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        return mToolbar;
+    }
+}
---
+++ app/src/main/java/org/seanmu13/flickrbrowser/MainActivity.java
@@ -0,0 +1,87 @@
+package org.seanmu13.flickrbrowser;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class MainActivity extends BaseActivity {
+
+    private static final String LOG_TAG = "MainActivity";
+    private List<Photo> mPhotosList = new ArrayList<Photo>();
+    private RecyclerView mRecyclerView;
+    private FlickrRecyclerViewAdapter flickrRecyclerViewAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        activateToolbar();
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.recycler_view);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        ProcessPhotos processPhotos = new ProcessPhotos("android,lollipop",true);
+        processPhotos.execute();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        if(id == R.id.menu_search) {
+            Intent intent = new Intent(this,SearchActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public class ProcessPhotos extends GetFlickrJsonData {
+
+        public ProcessPhotos(String searchCriteria, boolean matchAll) {
+            super(searchCriteria, matchAll);
+        }
+
+        public void execute () {
+            super.execute();
+            ProcessData processData = new ProcessData();
+            processData.execute();
+        }
+
+        public class ProcessData extends DownloadJsonData {
+
+            @Override
+            protected void onPostExecute(String webData) {
+                super.onPostExecute(webData);
+                flickrRecyclerViewAdapter = new FlickrRecyclerViewAdapter(MainActivity.this, getmPhotos());
+                mRecyclerView.setAdapter(flickrRecyclerViewAdapter);
+            }
+        }
+    }
+
+}
