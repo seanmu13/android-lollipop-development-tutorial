956e152be993d4ebbe7ec172c7d47854417fa5be
Sean Murray <seanmu13@gmail.com>
2015-03-13 20:01:57 -0400

    Lecture 133 - Completed Photo Details Layout, Completed App

--- app/src/main/java/org/seanmu13/flickrbrowser/GetFlickrJsonData.java
+++ app/src/main/java/org/seanmu13/flickrbrowser/GetFlickrJsonData.java
@@ -77,11 +77,12 @@ public class GetFlickrJsonData extends GetRawData {
                 String title = jsonPhoto.getString(FLICKR_TITLE);
                 String author = jsonPhoto.getString(FLICKR_AUTHOR);
                 String authorID = jsonPhoto.getString(FLICKR_AUTHOR_ID);
-                String link = jsonPhoto.getString(FLICKR_LINK);
+                //String link = jsonPhoto.getString(FLICKR_LINK);
                 String tags = jsonPhoto.getString(FLICKR_TAGS);
 
                 JSONObject jsonMedia = jsonPhoto.getJSONObject(FLICKR_MEDIA);
                 String photoUrl = jsonMedia.getString(FLICKR_PHOTO_URL);
+                String link = photoUrl.replaceFirst("_m.","_b.");
 
                 Photo photoObject = new Photo(title, author, authorID, link, tags, photoUrl);
                 this.mPhotos.add(photoObject);
