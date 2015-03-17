d184158fdc6415c8bc933f249564e940e75fdccc
Sean Murray <seanmu13@gmail.com>
2015-03-17 12:45:55 -0400

    Initial commit

---
+++ app/src/main/java/org/example/android/contactresolverexample/MainActivity.java
@@ -0,0 +1,46 @@
+package org.example.android.contactresolverexample;
+
+import android.app.ListActivity;
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ListActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ContentResolver cr = getContentResolver();
+        Cursor c = cr.query(ContactsContract.Contacts.CONTENT_URI,
+                new String[] { ContactsContract.Contacts.DISPLAY_NAME},
+                null,null,null);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
