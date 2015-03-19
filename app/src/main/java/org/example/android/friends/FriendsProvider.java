71e356f281c488a2adefbe8b4f7fc79558df6d32
Sean Murray <seanmu13@gmail.com>
2015-03-18 23:10:44 -0400

    Lecture 158 - Setup of the Content Provider Class

--- app/src/main/java/org/example/android/friends/FriendsProvider.java
+++ app/src/main/java/org/example/android/friends/FriendsProvider.java
@@ -1,7 +1,51 @@
 package org.example.android.friends;
 
+import android.content.ContentProvider;
+import android.content.UriMatcher;
+import android.net.Uri;
+
 /**
  * Created by seanmu13 on 3/18/2015.
  */
-public class FriendsProvider {
+public class FriendsProvider extends ContentProvider {
+    private FriendsDatabase mOpenHelper;
+
+    private static String TAG = FriendsProvider.class.getSimpleName();
+    private static final UriMatcher sUriMatcher = buildUriMatcher();
+
+    private static final int FRIENDS = 100;
+    private static final int FRIENDS_ID = 101;
+
+    private static UriMatcher buildUriMatcher() {
+        final UriMatcher matcher = new UriMatcher(UriMatcher.NO_MATCH);
+        final String authority = FriendsContract.CONTENT_AUTHORITY;
+        matcher.addURI(authority, "friends", FRIENDS);
+        matcher.addURI(authority, "friends/*", FRIENDS_ID);
+        return matcher;
+    }
+
+    @Override
+    public boolean onCreate() {
+        mOpenHelper = new FriendsDatabase(getContext());
+        return true;
+    }
+
+    private void deleteDatabase(){
+        mOpenHelper.close();
+        FriendsDatabase.deleteDatabase(getContext());
+        mOpenHelper = new FriendsDatabase(getContext());
+    }
+
+    @Override
+    public String getType(Uri uri) {
+        final int match = sUriMatcher.match(uri);
+        switch(match) {
+            case FRIENDS:
+                return FriendsContract.Friends.CONTENT_TYPE;
+            case FRIENDS_ID:
+                return FriendsContract.Friends.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalArgumentException("Unknown Uri: " + uri);
+        }
+    }
 }
