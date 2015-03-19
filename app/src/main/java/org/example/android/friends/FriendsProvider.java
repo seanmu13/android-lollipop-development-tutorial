11c06fa4dcd471c1e8bc7af2af86121db98251da
Sean Murray <seanmu13@gmail.com>
2015-03-19 12:59:19 -0400

    Lecture 159 - Create the Insert and Query Methods for Content Provider

--- app/src/main/java/org/example/android/friends/FriendsProvider.java
+++ app/src/main/java/org/example/android/friends/FriendsProvider.java
@@ -1,8 +1,14 @@
 package org.example.android.friends;
 
 import android.content.ContentProvider;
+import android.content.ContentValues;
 import android.content.UriMatcher;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+import android.provider.BaseColumns;
+import android.util.Log;
 
 /**
  * Created by seanmu13 on 3/18/2015.
@@ -30,7 +36,7 @@ public class FriendsProvider extends ContentProvider {
         return true;
     }
 
-    private void deleteDatabase(){
+    private void deleteDatabase() {
         mOpenHelper.close();
         FriendsDatabase.deleteDatabase(getContext());
         mOpenHelper = new FriendsDatabase(getContext());
@@ -39,11 +45,50 @@ public class FriendsProvider extends ContentProvider {
     @Override
     public String getType(Uri uri) {
         final int match = sUriMatcher.match(uri);
-        switch(match) {
+        switch (match) {
             case FRIENDS:
                 return FriendsContract.Friends.CONTENT_TYPE;
             case FRIENDS_ID:
                 return FriendsContract.Friends.CONTENT_ITEM_TYPE;
+            default:
+                throw new IllegalArgumentException("Unknown Uri: " + uri);
+        }
+    }
+
+    @Override
+    public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs, String sortOrder) {
+        final SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+        final int match = sUriMatcher.match(uri);
+
+        SQLiteQueryBuilder queryBuilder = new SQLiteQueryBuilder();
+        queryBuilder.setTables(FriendsDatabase.Tables.FRIENDS);
+
+        switch (match) {
+            case FRIENDS:
+                // do nothing
+                break;
+            case FRIENDS_ID:
+                String id = FriendsContract.Friends.getFriendId(uri);
+                queryBuilder.appendWhere(BaseColumns._ID + "=" + id);
+                break;
+            default:
+                throw new IllegalArgumentException("Unknown Uri: " + uri);
+        }
+
+        Cursor cursor = queryBuilder.query(db, projection, selection, selectionArgs, null, null, sortOrder);
+        return cursor;
+    }
+
+    @Override
+    public Uri insert(Uri uri, ContentValues values) {
+        Log.v(TAG, "insert(uri=" + uri + ", values=" + values.toString());
+        final SQLiteDatabase db = mOpenHelper.getReadableDatabase();
+        final int match = sUriMatcher.match(uri);
+
+        switch (match) {
+            case FRIENDS:
+                long recordId = db.insertOrThrow(FriendsDatabase.Tables.FRIENDS, null, values);
+                return FriendsContract.Friends.buildFriendUri(String.valueOf(recordId));
             default:
                 throw new IllegalArgumentException("Unknown Uri: " + uri);
         }
