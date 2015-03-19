f4c27415081b7b859d121ce37e0a6782440f14b9
Sean Murray <seanmu13@gmail.com>
2015-03-19 15:59:16 -0400

    Lecture 165 - Created FriendsListFragment

--- app/src/main/java/org/example/android/friends/FriendsListLoader.java
+++ app/src/main/java/org/example/android/friends/FriendsListLoader.java
@@ -1,11 +1,11 @@
 package org.example.android.friends;
 
-import android.content.AsyncTaskLoader;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
 import android.net.Uri;
 import android.provider.BaseColumns;
+import android.support.v4.content.AsyncTaskLoader;
 import android.util.Log;
 
 import java.util.ArrayList;
