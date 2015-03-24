a775889e5ee93167595d26ff3ff2e8f38c36f4c0
Sean Murray <seanmu13@gmail.com>
2015-03-24 12:42:41 -0400

    Lecture 169 - Work on the MainActivity Class

--- app/src/main/java/org/example/android/friends/FriendsDialog.java
+++ app/src/main/java/org/example/android/friends/FriendsDialog.java
@@ -20,6 +20,7 @@ public class FriendsDialog extends DialogFragment {
     private LayoutInflater mLayoutInflater;
     public static final String DIALOG_TYPE = "command";
     public static final String DELETE_RECORD = "deleteRecord";
+    public static final String DELETE_DATABASE = "deleteDatabase";
 
     @NonNull
     @Override
@@ -38,6 +39,20 @@ public class FriendsDialog extends DialogFragment {
                 public void onClick(DialogInterface dialog, int which) {
                     ContentResolver contentResolver = getActivity().getContentResolver();
                     Uri uri = FriendsContract.Friends.buildFriendUri(String.valueOf(_id));
+                    contentResolver.delete(uri, null, null);
+                    Intent intent = new Intent(getActivity().getApplicationContext(), MainActivity.class);
+                    intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                    startActivity(intent);
+                }
+            });
+        } else if(command.equals(DELETE_DATABASE)){
+            TextView popupMessage = (TextView) view.findViewById(R.id.popup_message);
+            popupMessage.setText("Are you sure you wish to delete the entire database?");
+            builder.setView(view).setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    ContentResolver contentResolver = getActivity().getContentResolver();
+                    Uri uri = FriendsContract.URI_TABLE;
                     contentResolver.delete(uri, null, null);
                     Intent intent = new Intent(getActivity().getApplicationContext(), MainActivity.class);
                     intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
